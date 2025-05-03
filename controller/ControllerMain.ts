cc929c9a19592d102ea521c3b17a92e884c21b6f
skx1322 <kaixing636@gmail.com>
2025-05-04 00:20:41 +0800

    Improved TSX and added view directory/file, UI work in progress and more API work in progress

--- controller/ControllerMain.ts
+++ controller/ControllerMain.ts
@@ -2,7 +2,7 @@ import { Elysia, t } from "elysia";
 import { CipText, ImageCip } from "../utils/ImageConfig";
 import { uploadImage } from "../utils/fs/uploadFile";
 import { CreateFolder } from "../utils/fs/FolderConfig";
-import { getFolderInFolder } from "../utils/fs/folderList";
+import { getFilesInFolder, getFolderInFolder } from "../utils/fs/folderList";
 import generateUUID from "../utils/crypto/UUID";
 
 type fileFormat = "png" | "jpg" | "gif" | "txt" | "";
@@ -18,128 +18,164 @@ interface FileObject {
 }
 
 export const upload = new Elysia()
-.get("/api/StorageFolder", async({set})=>{
-    const folderList = await getFolderInFolder("./storage");
-    return{
-        success: true,
-        output: folderList,
-    }
-})
-.post('/api/createFolder', async({body, set})=>{
-    const {name} = body;
-    const UUID = await generateUUID();
-    try {
-        await CreateFolder(`./storage/`, `${name}_${UUID}`)
-        set.status = 200; 
-        return{
-            success: true,
-            message: "Successfully created a folder.",
-            status: set.status,    
-        }
-    } catch (error) {
-        set.status = 500;
-        return{
-            success: false,
-            message: "Failed to create folder.",
-            status: set.status,    
-            output: error,
-        }
-    }
-},{
-    body: t.Object({
-        name: t.String(),
-    })
-})
-.post('/api/cipherText', async({body, set})=>{
-    try {
-        const { name, fileType ,passKey, fileDirectory } = body;
-
-        const TextPlayload: FileObject = {
-            FileName: name, 
-            InitialPoint: `./storage/${fileDirectory}/`,
-            EndPoint: `./storage/${fileDirectory}/`,
-            Format: fileType || "jpg",
-            // CustomOutput: name, 
-            Encrypt: true, 
-            SecretKey: passKey,
-        }
-
-        await CipText(TextPlayload)
-    } catch (error) {
-        set.status = 500;
-        return{
-            success: false,
-            message: "Failed to cipher text.",
-            status: set.status,    
-            output: error,
-        }
-    }
-},{
-    body: t.Object({
-        name: t.String(),
-        fileType: t.Optional(t.UnionEnum(["png" ,"jpg",  "gif" , "txt", ""])),
-        passKey: t.Optional(t.String()),
-        fileDirectory: t.String(),
-    })
-})
-.post('/api/uploadImage', async ({body, set}) => {
-    const { name, file, passKey, fileDirectory } = body;
-    
-    interface FileReturn{
-        directory: string,
-        fileType: fileFormat,
-        fileSize: number,
-        uploaded: boolean,
-    }
-    try {
-        const filePayload = {
-            name: name,
-            file: file,
-            directory: fileDirectory,
-        }
-        const uploaded: FileReturn = await uploadImage(filePayload);
-
-        if(!uploaded.uploaded){
-            set.status = 400;
-            return{
-                success: false,
-                message: "Failed to upload image.",
-                status: set.status,    
-                output: uploaded,
+    .get("/api/StorageFolder", async ({ set }) => {
+        try {
+            const folderList = await getFolderInFolder("./storage");
+            set.status = 200;
+            return {
+                success: true,
+                message: "Successfully retrieved storage data.",
+                status: set.status,
+                output: folderList,
+            }
+        } catch (error) {
+            return {
+                success: true,
+                message: "Successfully retrieved storage data.",
+                status: set.status,
+                output: error,
             }
         }
-        const {directory, fileType, fileSize } = uploaded
-        const cryptoPayload: FileObject = {
-            FileName: `${name}.${fileType}`,
-            InitialPoint: fileDirectory,
-            EndPoint: directory,
-            Format: "txt",
-            CustomOutput: name,
-            Encrypt: true,
-            SecretKey: passKey,
-        }
-        await ImageCip(cryptoPayload);
-        set.status = 200;
-        return{
-            success: true,
-            message: "Successfully stored image.",
-            status: set.status,    
-            output: cryptoPayload.FileName,
-        }
-    } catch (error) {
-        set.status = 500;
-        return{
-            success: false,
-            message: "Failed to store image.",
-            status: set.status,    
-            output: error,
-        }
-    }
-},{
-    body: t.Object({
-        name: t.String(),
-        file: t.File(),
-        passKey: t.Optional(t.String()),
-        fileDirectory: t.String(),
     })
-})
+    .get("/api/listFile/:directoryID", async ({ params, set }) => {
+        const { directoryID } = params;
+        const file = await getFilesInFolder(directoryID);
+        if (!file) {
+            set.status = 400;
+            return {
+                success: false,
+                message: `Failed to retrieve data.`,
+                status: set.status,
+                output: file,
+            }
+        }
+        set.status = 200;
+        return {
+            success: true,
+            message: `Successfully retrieved file data from ${directoryID}.`,
+            status: set.status,
+            output: file,
+        }
+    }, {
+        params: t.Object({
+            directoryID: t.String(),
+        })
+    })
+    .post('/api/createFolder', async ({ body, set }) => {
+        const { name } = body;
+        const UUID = await generateUUID();
+        try {
+            await CreateFolder(`./storage/`, `${name}_${UUID}`)
+            set.status = 200;
+            return {
+                success: true,
+                message: "Successfully created a folder.",
+                status: set.status,
+            }
+        } catch (error) {
+            set.status = 500;
+            return {
+                success: false,
+                message: "Failed to create folder.",
+                status: set.status,
+                output: error,
+            }
+        }
+    }, {
+        body: t.Object({
+            name: t.String(),
+        })
+    })
+    .post('/api/cipherText', async ({ body, set }) => {
+        try {
+            const { name, fileType, passKey, fileDirectory } = body;
+
+            const TextPlayload: FileObject = {
+                FileName: name,
+                InitialPoint: `./storage/${fileDirectory}/`,
+                EndPoint: `./storage/${fileDirectory}/`,
+                Format: fileType || "jpg",
+                // CustomOutput: name, 
+                Encrypt: true,
+                SecretKey: passKey,
+            }
+
+            await CipText(TextPlayload)
+        } catch (error) {
+            set.status = 500;
+            return {
+                success: false,
+                message: "Failed to cipher text.",
+                status: set.status,
+                output: error,
+            }
+        }
+    }, {
+        body: t.Object({
+            name: t.String(),
+            fileType: t.Optional(t.UnionEnum(["png", "jpg", "gif", "txt", ""])),
+            passKey: t.Optional(t.String()),
+            fileDirectory: t.String(),
+        })
+    })
+    .post('/api/uploadImage', async ({ body, set }) => {
+        const { name, file, passKey, fileDirectory } = body;
+
+        interface FileReturn {
+            directory: string,
+            fileType: fileFormat,
+            fileSize: number,
+            uploaded: boolean,
+        }
+        try {
+            const filePayload = {
+                name: name,
+                file: file,
+                directory: fileDirectory,
+            }
+            const uploaded: FileReturn = await uploadImage(filePayload);
+
+            if (!uploaded.uploaded) {
+                set.status = 400;
+                return {
+                    success: false,
+                    message: "Failed to upload image.",
+                    status: set.status,
+                    output: uploaded,
+                }
+            }
+            const { directory, fileType, fileSize } = uploaded
+            const cryptoPayload: FileObject = {
+                FileName: `${name}.${fileType}`,
+                InitialPoint: fileDirectory,
+                EndPoint: directory,
+                Format: "txt",
+                CustomOutput: name,
+                Encrypt: true,
+                SecretKey: passKey,
+            }
+            await ImageCip(cryptoPayload);
+            set.status = 200;
+            return {
+                success: true,
+                message: "Successfully stored image.",
+                status: set.status,
+                output: cryptoPayload.FileName,
+            }
+        } catch (error) {
+            set.status = 500;
+            return {
+                success: false,
+                message: "Failed to store image.",
+                status: set.status,
+                output: error,
+            }
+        }
+    }, {
+        body: t.Object({
+            name: t.String(),
+            file: t.File(),
+            passKey: t.Optional(t.String()),
+            fileDirectory: t.String(),
+        })
+    })
--- utils/fs/folderList.ts
+++ utils/fs/folderList.ts
@@ -3,50 +3,65 @@ import { join } from "path";
 import { readdir } from "fs/promises";
 import { lstat } from "fs/promises";
 
-
-export async function getFilesInFolder(folderPath: string): Promise<string[]> {
-    try {
-        const items = readdirSync(folderPath);
-        const files: string[] = items.filter((item) => {
-            const itemPath = join(folderPath, item);
-            return statSync(itemPath).isFile();
-        });
-        return files;
-    } catch (error) {
-        console.error(`Unable to read folder ${error}`)
-        return [];
-    }
+interface FileObject {
+  fileName: string,
+  fileDate: string,
+  fileSize: number,
+  fileDirectory: string,
+  fileReceived: boolean;
 }
 
-interface folderInfo{
-    folderName: string,
-    folderDirectory: string,
-    fileNum: number,
+export async function getFilesInFolder(folderPath: string): Promise<FileObject[]> {
+  try {
+    const items = readdirSync(`./storage/${folderPath}/`);
+    const fileObjects: FileObject[] = items.map(file => {
+      const fullPath = `./storage/${folderPath}/${file}`;
+      const stats = statSync(fullPath)
+      return {
+        fileName: file,
+        fileDate: stats.mtime.toISOString(), 
+        fileSize: stats.size, 
+        fileDirectory: fullPath, 
+        fileReceived: true,
+      };
+
+    })
+
+    return fileObjects;
+  } catch (error) {
+    console.error(`Unable to read folder ${error}`)
+    return [];
+  }
+}
+
+
+interface folderInfo {
+  folderName: string,
+  folderDirectory: string,
+  fileNum: number,
 }
 
 export async function getFolderInFolder(folderPath: string) {
-    try {
-      const folders = await readdir(folderPath);
-      const folderData: folderInfo[] = [];
+  try {
+    const folders = await readdir(folderPath);
+    const folderData: folderInfo[] = [];
 
-      for(const folder of folders){
-        const folderDirectory = `${folderPath}/${folder}`;
-        const stats = await lstat(folderDirectory);
+    for (const folder of folders) {
+      const folderDirectory = `${folderPath}/${folder}`;
+      const stats = await lstat(folderDirectory);
 
-        if (stats.isDirectory()) {
-            const fileCount = (await readdir(folderDirectory)).length;
-            folderData.push({
-                folderName: folder.split("_")[0], // Extract folder name before "_"
-                folderDirectory: folder, // Keep the full folder name as an identifier
-                fileNum: fileCount
-            });
-        }
+      if (stats.isDirectory()) {
+        const fileCount = (await readdir(folderDirectory)).length;
+        folderData.push({
+          folderName: folder.split("_")[0], // Extract folder name before "_"
+          folderDirectory: folder, // Keep the full folder name as an identifier
+          fileNum: fileCount
+        });
       }
-      return folderData;
-    } catch (error) {
-      console.error("Error reading directory:", error);
-      return [];
     }
+    return folderData;
+  } catch (error) {
+    console.error("Error reading directory:", error);
+    return [];
   }
-
-console.log(await getFolderInFolder("./storage"))
+}
